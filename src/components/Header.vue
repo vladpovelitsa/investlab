9f1e776788f9833d99838920834102c8d2fe01a4
vladpovelitsa <vlad.povelitsa@gmail.com>
2021-04-14 10:39:33 +0300

    unpdate header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,13 +2,13 @@
 	<v-app-bar app color="#132A46" dark>
 		<v-container>
 			<v-row align="center">
-				<v-col cols="1" col-md="4"></v-col>
-				<v-col cols="5" cols-md="4" align="center">
+				<v-col cols="1" sm="4"></v-col>
+				<v-col cols="5" sm="4" align="center">
 					<router-link :to="{ name: 'home' }">
 						<img src="./../assets/img/logo.svg" width="130" />
 					</router-link>
 				</v-col>
-				<v-col cols="5" cols-md="4">
+				<v-col cols="5" sm="4">
 					<router-link :to="{ name: 'login' }" class="d-flex justify-end">
 						<img src="./../assets/img/avatar.png" width="40" height="40" />
 					</router-link>
